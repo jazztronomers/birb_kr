13ad2271bbb8d5358abd7804c99ad2360fe46df7
rubenchu <gyb0418@gmail.com>
2022-04-09 18:41:19 +0900

    append inline ad component

--- app/static/js/advertisement.js
+++ app/static/js/advertisement.js
@@ -4,17 +4,7 @@ function initAd(){
         for (div of document.querySelectorAll('.ad.fixed')){
             console.log(div)
             div.style.display='flex';
-            div.innerHTML=`
-
-            <h3>광고주 모십니다</h3>
-
-            <span>- 탐조관련 컨텐츠 only</span>
-            <span>- pc노출 only</span>
-            <span>- 고정위치</span>
-            <span>- 200px * 800px</span>
-
-
-            `
+            div.innerHTML=getAdFixedVertical()
         }
 
     }
@@ -23,13 +13,14 @@ function initAd(){
 function getAdFixedVertical(){
 
     return `
-    <h3>광고주 모십니다</h3>
+    <H3><span style="font-size: 1.4rem">LOOKING FOR ADVERTISER</span></H3>
 
-    <span>- 탐조관련 컨텐츠 only</span>
-    <span>- pc노출 only</span>
-    <span>- 고정위치</span>
+    <span>- for birder only</span>
+    <span>- Ads here for PC only</span>
     <span>- 200px * 800px</span>
-
+    <hr>
+    <span style="font-size: .7rem">DM: @jazzbirb</span>
+    <span style="font-size: .7rem">Email: jazztronomers@gmail.com</span>
     `
 }
 
@@ -37,15 +28,26 @@ function getAdHorizontalGallery(){
 
     return `
     <div class="ad inline">
-        <div>
-            <span style="font-size: 1.2rem"> LOOKING FOR ADVERTISER</span>
-            <span style="font-size: .7rem">for birder only</span>
+        <div class="ad inline block">
+            <div>
+                <span style="font-size: 1.2rem">LOOKING FOR ADVERTISER</span>
+                <span style="font-size: .7rem">for birder only</span>
+
+            </div>
+            <div>
+                <span style="font-size: .7rem">ads here for both PC and Mobile</span>
+            </div>
+            <div>
+                <span style="font-size: .7rem">DM: @jazzbirb</span>
+            </div>
+            <div>
+                <span style="font-size: .7rem">Email: jazztronomers@gmail.com</span>
+            </div>
         </div>
-        <div>
-            <span style="font-size: .7rem">DM: @jazzbirb</span>
-        </div>
-        <div>
-            <span style="font-size: .7rem">Email: jazztronomers@gmail.com</span>
+        <div class="ad inline block">
+            <div>새가 사진보다 소중합니다</div>
+            <div>새를 괴롭히면서까지 사진을 찍지 마세요</div>
+            <div>있는 그대로의 자연을 즐깁시다</div>
         </div>
     </div>
     `
